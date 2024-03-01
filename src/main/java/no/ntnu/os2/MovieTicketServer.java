492cccdc2a77acf012c2d15c2deaa094632703c8
SmileyFace799 <Henrikgran85@gmail.com>
2024-03-01 13:03:15 +0100

    Started lol

--- src/main/java/no/ntnu/os2/MovieTicketServer.java
+++ src/main/java/no/ntnu/os2/MovieTicketServer.java
@@ -1,21 +1,34 @@
 package no.ntnu.os2;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class MovieTicketServer {
+	private static final Logger LOGGER = Logger.getLogger(MovieTicketServer.class.getName());
+
 	private final String movieName;
 	private final int totalSeats;
+
+	private int availableSeats;
 
 	public MovieTicketServer(String movieName, int totalSeats) {
 		this.movieName = movieName;
 		this.totalSeats = totalSeats;
+		this.availableSeats = totalSeats;
 	}
 
-	public void bookTicket(int numberOfSeats) {
-		// TODO: Write code
+	public void bookTicket(String name, int numberOfTickets) {
+		LOGGER.log(
+				Level.INFO,
+				"Booker name: {0} | Available seats: {1}",
+				new Object[]{name, availableSeats}
+		);
 
-		if ((availableSeats - numberOfSeats) < 0) {
-			// TODO: Write code
+		if ((availableSeats - numberOfTickets) < 0) {
+			LOGGER.warning("Not enough available seats");
+		} else {
+			availableSeats -= numberOfTickets;
+			LOGGER.log(Level.INFO, "Successfully booked {0} tickets", numberOfTickets);
 		}
-
-		// TODO: Write code
 	}
 }
